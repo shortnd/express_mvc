93097e4e8ea78864b0a304c0a2bf21593c82ac75
shortnd <ococncol@gmail.com>
2018-09-06 22:01:04 -0400

    messing around with view files

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 module.exports.controller = (app) => {
   // Get user infomation
   app.get('/users', (req, res) => {
-    res.render('index', {title: 'Users'});
+    res.render('user', {title: 'Users', description: 'This is the description of all the users'});
   });
 }
